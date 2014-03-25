dfb7d9235069ebecd55b79f4388e5d3c33798e64
zn4ke <znakebein@gmail.com>
2014-03-25 15:52:24 +0000

    update

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,31 +1,31 @@
 'use strict';
 
 angular.module('studiApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.info='NavbarCtrl';
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }, {
-      'title': 'Enter',
-      'link': '/enter'
-    }, {
-      'title': 'State1',
-      'link': '/state1'
-    }, {
-      'title': 'Admin',
-      'link': '/admin',
-      'access': 'admin'
-    }];
-    
-    $scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+    .controller('NavbarCtrl', function ($scope, $location, Auth) {
+        $scope.info='NavbarCtrl';
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
+        }, {
+            'title': 'Enter',
+            'link': '/enter'
+        }, {
+            'title': 'State1',
+            'link': '/state1'
+        }, {
+            'title': 'Admin',
+            'link': '/admin/decks',
+            'access': 'admin'
+        }];
+        
+        $scope.logout = function() {
+          Auth.logout()
+                .then(function() {
+                $location.path('/login');
+            });
+        };
+        
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+    });
---
+++ app/scripts/directives/ckeditor.js
@@ -0,0 +1,103 @@
+'use strict';
+
+angular.module('studiApp')
+    .directive('ckEditor', function () {
+        return {
+            require: '?ngModel',
+            link: function ($scope, elm, attr, ngModel) {
+
+                var ck = CKEDITOR.replace(elm[0]);
+
+                ck.on('pasteState', function () {
+                    $scope.$apply(function () {
+                        ngModel.$setViewValue(ck.getData());
+                    });
+                });
+
+                ngModel.$render = function (value) {
+                    ck.setData(ngModel.$modelValue);
+                };
+            }
+        };
+    });
+
+
+
+
+
+
+// function myCtrl($scope){
+//     $scope.ckEditors = [];
+//     $scope.addEditor = function(){
+//         var rand = ""+(Math.random() * 10000);
+//         $scope.ckEditors.push({value:rand});
+//     }
+// }
+
+// https://github.com/ericpanorel/AngularCkEdDirective
+
+// "use strict";
+// ckApp.directive('ckedit', function ($parse) {
+//     CKEDITOR.disableAutoInline = true;
+//     var counter = 0,
+//     prefix = '__ckd_';
+
+//     return {
+//         restrict: 'A',
+//         link: function (scope, element, attrs, controller) {
+//             var getter = $parse(attrs.ckedit),
+//                 setter = getter.assign;
+      
+//             attrs.$set('contenteditable', true); // inline ckeditor needs this
+//             if (!attrs.id) {
+//                 attrs.$set('id', prefix + (++counter));
+//             }
+
+//             // CKEditor stuff
+//             // Override the normal CKEditor save plugin
+
+//             CKEDITOR.plugins.registered['save'] =
+//             {
+//                 init: function (editor) {
+//                     editor.addCommand('save',
+//                         {
+//                             modes: { wysiwyg: 1, source: 1 },
+//                             exec: function (editor) {
+//                                 if (editor.checkDirty()) {
+//                                     var ckValue = editor.getData();
+//                                     scope.$apply(function () {
+//                                         setter(scope, ckValue);
+//                                     });
+//                                     ckValue = null;
+//                                     editor.resetDirty();
+//                                 }
+//                             }
+//                         }
+//                     );
+//                     editor.ui.addButton('Save', { label: 'Save', command: 'save', toolbar: 'document' });
+//                 }
+//             };
+//             var options = {};
+//             options.on = {
+//                 blur: function (e) {
+//                     if (e.editor.checkDirty()) {
+//                         var ckValue = e.editor.getData();
+//                         scope.$apply(function () {
+//                             setter(scope, ckValue);
+//                         });
+//                         ckValue = null;
+//                         e.editor.resetDirty();
+//                     }
+//                 }
+//             };
+//             options.extraPlugins = 'sourcedialog';
+//             options.removePlugins = 'sourcearea';
+//             var editorangular = CKEDITOR.inline(element[0], options); //invoke
+
+//             scope.$watch(attrs.ckedit, function (value) {
+//                 editorangular.setData(value);
+//             });
+//         }
+//     }
+    
+// });
