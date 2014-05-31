eab2b24e25ae15d485d5778776946da404879b61
zn4ke <znakebein@gmail.com>
2014-05-31 01:12:45 +0100

    ......

--- app/scripts/chemframe.js
+++
@@ -1,6 +0,0 @@
-var sketcher = new ChemDoodle.SketcherCanvas('mychemsketcher', 500, 300, {
-    useServices:false,
-    oneMolecule:true
-});
-console.log('mychemsketcher',document.getElementById('mychemsketcher'))
-sketcher.repaint()
